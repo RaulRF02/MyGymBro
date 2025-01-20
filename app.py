40c03df0d86daad0313958299d96e6ee2cd9ca3c
RaulRF02 <raulrf2601@gmail.com>
2025-01-20 19:07:00 +0100

    Revert "Otra prueba de configuración Heroku"
    This reverts commit a0cb205edbf9424db8c2f54ceeaadb7d4bb22364.

--- app.py
+++ app.py
@@ -17,43 +17,13 @@ def create_app():
         app, config=app.config["SWAGGER"], template_file="docs/swagger.yaml"
     )
 
-    # Crear tablas solo si no existen
-    with app.app_context():
-        inspect_and_create_tables(app)
-
-    # Registrar blueprints
-    register_blueprints(app)
-
-    return app
-
-
-def inspect_and_create_tables(app):
-    """
-    Inspecciona las tablas existentes en la base de datos
-    y las crea solo si no están presentes.
-    """
-    from sqlalchemy import inspect
-
-    inspector = inspect(db.engine)
-    existing_tables = inspector.get_table_names()
-
     from models.exercise_model import Exercise
     from models.routine_model import Routine
     from models.routine_exercise import RoutineExercise
     from models.training_plan_model import TrainingPlan
     from models.progress_model import Progress
 
-    # Lista de todas las tablas definidas en tu proyecto
-    tables_to_check = [
-        Exercise.__tablename__,
-        Routine.__tablename__,
-        RoutineExercise.__tablename__,
-        TrainingPlan.__tablename__,
-        Progress.__tablename__,
-    ]
+    # Register blueprints
+    register_blueprints(app)
 
-    # Crear tablas solo si no existen
-    for table in tables_to_check:
-        if table not in existing_tables:
-            db.create_all()
-            break
+    return app
